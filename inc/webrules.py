6c52b61578256e6cbbdfa2df74926e04c7ca49ae
Rob Goetz <rgoetz@tagged.com>
2012-11-07 21:08:42 -0800

    add webrule for checking values on user

--- inc/webrules.py
+++ inc/webrules.py
@@ -1,16 +1,24 @@
 import web
+from models.user import user
 
 class must:
 
     @staticmethod
     def be_logged_in(fn):
-        web.debug("LOG IN TEST")
         def func(*args):
-            web.debug("LOGG IN")
-            web.debug(web.ctx)
             if web.config.session.get('user', False):
                 return fn(*args)
             else:
                 raise web.seeother('/static/login.html')
         return func
 
+    @staticmethod
+    def have_value(value, responder):
+        def wrap(fn):
+            def func(*args):
+                user = web.config.session.user
+                if user.vals.get(value, False):
+                    return fn(*args)
+                return responder(*args)
+            return func
+        return wrap
