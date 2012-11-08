ac9a5cca5414de52f42b5cc4b0272cd6494bae3b
Rob Goetz <rgoetz@tagged.com>
2012-11-07 21:11:28 -0800

    add tutorial page

--- controllers/main.py
+++ controllers/main.py
@@ -4,6 +4,11 @@ from inc.webrules import must
 render = web.template.render('templates/')
 
 class actions:
+    @staticmethod
+    def tutorial(self):
+        return 'tutorial'
+
     @must.be_logged_in
+    @must.have_value('tutorial', actions.tutorial)
     def GET(self):
             return "main"
